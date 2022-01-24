b2b6ae5bd8274bf8ba9bf10bb4c2ac0af9667605
orelsgit <o.zilberman@constru.ai>
2022-01-24 12:00:11 +0200

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by Orel Zilberman on 24/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
